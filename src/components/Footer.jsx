41bc4d172735fb8c8fc82ccd56e68db4f9bb2f50
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-02-26 18:27:58 -0300

    feat: add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer() {
+    return (
+     <>
+         <footer className={"text-center text-[12px] lg:text-[15px]"}>
+             <hr className={"border-[#182f46]"}/>
+             <p className={"mt-3 text-slate-600"}>&copy; 2025 Rafael Gonzaga. Todos os direitos reservados.</p>
+         </footer>
+     </>
+    )
+}
+
+export default Footer
