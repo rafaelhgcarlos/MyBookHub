d91f1430459264bc413f52f71e909ae0bad9ea84
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-02-26 18:28:18 -0300

    feat: add home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header.jsx";
+import Hero from "../components/Hero.jsx";
+import Footer from "../components/Footer.jsx";
+
+function Home() {
+
+    return (
+        <>
+            <Header/>
+            <Hero/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Home
