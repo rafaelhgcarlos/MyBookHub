0faf2e6ffb234a685311b5eeefa2bf693875a793
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-02-26 18:28:32 -0300

    feat: add main component

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './assets/css/index.css';
+import Home from './pages/Home.jsx';
+import Register from "./pages/Register.jsx";
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </Router>
+    </StrictMode>
+);
