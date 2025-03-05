7e718d78d9bc06c39f59116067f983c776a3b3a8
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-03-05 14:37:07 -0300

    fix: route

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './assets/css/index.css';
 import Home from './pages/Home.jsx';
 import Register from "./pages/Register.jsx";
