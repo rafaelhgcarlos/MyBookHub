e5f3df509f9707bb8af1a6cd33e0eea9b2371148
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-03-05 13:53:07 -0300

    refactor: replaced <a> with React Router <Link> for Home and Library.

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -1,10 +1,12 @@
+import {Link} from "react-router-dom";
+
 function NavHeader() {
     return (
         <>
             <div className={"text-[20px] list-none text-white md:bg-[#26415E] md:pb-[5px] md:pt-[5px] md:pl-[25px] lg:pl-[80px] md:pr-[25px] lg:pr-[80px] md:rounded-[10px]"}>
                 <ul className={"md:flex md:gap-[40px]"}>
-                    <li><a href={""}><i className={"uil uil-home flex gap-[5px] items-center mt-[10px] md:m-0 hover:text-[#03122F] transition duration-500"}>Home</i></a></li>
-                    <li><a href={""}><i className={"uil uil-book flex gap-[5px] items-center mt-[10px] md:m-0 hover:text-[#03122F] transition duration-500"}>Library</i></a></li>
+                    <Link to={"/"} className={"uil uil-home flex gap-[5px] items-center mt-[10px] md:m-0 hover:text-[#03122F] transition duration-500"}>Home</Link>
+                    <Link to={"/library"} className={"uil uil-book flex gap-[5px] items-center mt-[10px] md:m-0 hover:text-[#03122F] transition duration-500"}>Library</Link>
                     <li><a href={""}><i className={"uil uil-question-circle flex gap-[5px] items-center mt-[10px] md:m-0 hover:text-[#03122F] transition duration-500"}>About</i></a></li>
                 </ul>
             </div>
