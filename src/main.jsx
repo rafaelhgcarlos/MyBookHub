972812e07f5631b1c287be811807c261641d425a
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-03-05 14:07:36 -0300

    fix: fix github host

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './assets/css/index.css';
 import Home from './pages/Home.jsx';
 import Register from "./pages/Register.jsx";
