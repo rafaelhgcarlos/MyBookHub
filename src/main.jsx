95c8231e27b17a20e8d47a26844d088c8952a30c
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-03-05 13:55:42 -0300

    feat: imported Library component for /library route.

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './assets/css/index.css';
 import Home from './pages/Home.jsx';
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
+import Library from "./pages/Library.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
                 <Route path="/" element={<Home />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/library" element={<Library />} />
             </Routes>
         </Router>
     </StrictMode>
